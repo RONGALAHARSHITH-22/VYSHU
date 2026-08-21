6f7f085a28d0e1d1d9ef1c35f66691b1ec0d6564
HARSHITH <harshithrongala07@gmail.com>
2026-08-21 15:18:30 +0530

    Add surprise and our video files directly to assets and update autoplay

--- cutie.js
+++ cutie.js
@@ -69,6 +69,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }, i * 200);
       }
     }
+    if (ourVideoPlayer) {
+      ourVideoPlayer.play().catch(() => {});
+    }
     showToast("🎉 YAYYY! I knew you loved me! 💖");
   }
 
